105a87965d1a1fb71fa3f9fc1556c4eab4124125
Ryang <ieas0156@gmail.com>
2021-04-20 00:07:17 +0800

    Http test

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('index', { user: 'Guest' , ip: req.ip});
+    res.render('index', { user: 'Guest'});
 });
 
 router.get('/favicon.ico', (req, res)=>{
     res.send('');
+});
+
+router.get('/:msg', (req, res)=>{
+    res.send(req.params.msg);
 });
 
 module.exports = router;
