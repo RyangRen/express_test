3391e15177ff2ffb3784d01c34ca919099d13b10
Ryang <ieas0156@gmail.com>
2021-04-23 13:13:55 +0800

    test

--- test.js
+++ test.js
@@ -1,11 +1,17 @@
-var obj = {
-    date: "2021-04-30 11:11:11",
-    flow: 5,
-    oxygen: 10
-};
+// var obj = {
+//     date: "2021-04-30 11:11:11",
+//     flow: 5,
+//     oxygen: 10
+// };
 
-function k (key) { 
-    console.log(obj[key]);
-}
-k('flow');
-// console.log(Object.keys(obj));
+// function k (key) { 
+//     console.log(obj[key]);
+// }
+// k('flow');
+// // console.log(Object.keys(obj));
+
+var objArray = [ { foo: 1, bar: 2}, { foo: 3, bar: 4}, { foo: 5, bar: 6} ];
+
+var result = objArray.map(a => (a.foo*10));
+
+console.log(result)
